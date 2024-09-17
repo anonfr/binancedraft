abed2dc90b41b1c862875a2890e2a9d46a3ef193
anonfr <thexo.ogog@gmail.com>
2024-09-18 04:04:28 +0530

    Integrate TON Connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+'use client'
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import TonWalletConnect from './components/TonWalletConnect'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <TonConnectUIProvider manifestUrl="https://violet-binding-sloth-805.mypinata.cloud/ipfs/QmayBCjVRUtGPE1yd7Y9f5VoXBrBLULahrYKMkAJTXX3k9">
       <div>
         <img src={viteLogo} className="logo" alt="Vite logo" />
         <img src={reactLogo} className="logo react" alt="React logo" />
@@ -28,7 +31,7 @@ function App() {
       <p className="read-the-docs">
         developing...
       </p>
-    </>
+    </TonConnectUIProvider>
   )
 }
 
